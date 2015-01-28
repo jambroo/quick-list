2ec00973b7a10a2a1dd44ddf78b67de62f4688c9
James Brooking <jambroo@gmail.com>
2015-01-28 16:18:58 +0000

    Populate script work.

--- populate.js
+++ populate.js
@@ -1,20 +1,20 @@
+// NodeJS script to pre-populate 'collection' collection on the solr search pattern.
 var solr = require('solr-client');
 
-var client = solr.createClient();
-
+var client = solr.createClient('localhost', 8983, 'collection');
 client.autoCommit = true;
 
 var entries = [];
 for(var i = 0; i <= 10 ; i++){
-   var entry = {
-       id: i,
-       name: "Entry "+ i,
-       start: i
-   }
-   entries.push(entry);
+ var entry = {
+   id: i,
+   name: "Entry "+ i,
+   start: i
+ }
+ entries.push(entry);
 }
 
 client.add(entries, function(error, object){
-   console.log("Error: ", error);
-   console.log("Object: ", object);
+ console.log("Error: ", error);
+ console.log("Object: ", object);
 });
