aa74ba3088121f09b0878b38d9ad5f550d59f55c
James Brooking <jambroo@gmail.com>
2015-01-28 16:39:26 +0000

    Population script.

--- populate.js
+++ populate.js
@@ -1,20 +1,36 @@
 // NodeJS script to pre-populate 'collection' collection on the solr search pattern.
+const SAMPLE_DATA_LIMIT = 100000000;
+const START_END_DIFFERENCE = 5;
+
 var solr = require('solr-client');
 
 var client = solr.createClient('localhost', 8983, 'collection');
 client.autoCommit = true;
 
+// Remove all documents
+client.deleteByQuery("id:*", function(error, object) {
+    console.log("Error: ", error);
+    console.log("Object: ", object);
+});
+
+// Add entries
 var entries = [];
-for(var i = 0; i <= 10 ; i++){
- var entry = {
-   id: i,
-   name: "Entry "+ i,
-   start: i
- }
- entries.push(entry);
+for (var i = 0; i <= SAMPLE_DATA_LIMIT; i++) {
+    var entry = {
+        id: i,
+        name: "Entry " + i,
+        start: i
+    }
+
+    // 10% chance there is an end value set
+    if (Math.random() <= 0.1) {
+        entry.end = (i + Math.floor(Math.random() * (START_END_DIFFERENCE - 1) + 1));
+    }
+
+    entries.push(entry);
 }
 
-client.add(entries, function(error, object){
- console.log("Error: ", error);
- console.log("Object: ", object);
+client.add(entries, function(error, object) {
+    console.log("Error: ", error);
+    console.log("Object: ", object);
 });
