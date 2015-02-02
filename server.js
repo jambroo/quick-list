c67207010fcc7ddc2371cf943f8c9fbb113a14a0
James Brooking <jambroo@gmail.com>
2015-02-02 16:00:07 +0000

    Added search API function.

--- server.js
+++ server.js
@@ -9,6 +9,34 @@ app.get('/', function(req, res) {
   res.render('index');
 })
 
+app.get('/search', function(req, res) {
+  if (!req.query.q) {
+    res.render('view', {
+      results: null
+    });
+    return;
+  }
+
+  var start = 0;
+  if (req.query.start) {
+    start = req.query.start;
+  }
+
+  var solrPromise = lookup.query(req.query.q, start, 100)
+    .then(function(data) {
+      res.render('view', {
+        results: data.docs,
+        numFound: data.numFound
+      });
+      return;
+    }, function(err) {
+      res.render('view', {
+        results: null
+      });
+      return;
+    });
+})
+
 var server = app.listen(3000, function() {
   var port = server.address().port
 
